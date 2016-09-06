dfca1bb530398fecc09b59a2700a738fb58e0b9d
Hannes Buchwald <hb052@hdm-stuttgart.de>
2016-09-06 09:13:51 +0200

    Clean up and comment

--- Mobile_Application/app/src/main/java/org/hdm/app/timetracker/main/MainActivity.java
+++ Mobile_Application/app/src/main/java/org/hdm/app/timetracker/main/MainActivity.java
@@ -6,11 +6,13 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Window;
@@ -20,7 +22,6 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.hdm.app.timetracker.IntroActivity;
 import org.hdm.app.timetracker.R;
 import org.hdm.app.timetracker.datastorage.ActivityObject;
 import org.hdm.app.timetracker.datastorage.DataManager;
@@ -47,72 +48,189 @@ public class MainActivity extends Activity  {
     /**
      * Constants
      */
-    public static final int NOTIFICATION_ID = 100;
+    Handler handler = new Handler();
+    private Variables var;
+
 
     /**
      * Attributes
      */
-    int startMin = 0;
-    int startHour = 6;
-    int endHour = 24;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       // initFirstStart();
         initConfiguration();
         initCalenderMap();
         initDataLogger();
 //        initResetRecordedData();
         loadSavedObjectState();
-        setFullScreen(true);
-        setContentView(R.layout.activity_main);
-        
+        initLayout();
+    }
 
 
-        Log.d(TAG, "ImageSize " + DataManager.getInstance().imageMap.size());
 
+    @Override
+    public void onBackPressed() {
+        // ToDo write test cases
+
+        // if backPress is true then backpress button in active
+        if(Variables.getInstance().backPress) {
+
+            // if backPressDialog is true then show a AlertDialog before quit the app
+            if(Variables.getInstance().backPressDialog) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Really Exit?")
+                        .setMessage("Are you sure you want to exit?")
+                        .setNegativeButton(android.R.string.no, null)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                MainActivity.super.onBackPressed();
+                            }
+                        }).create().show();
+            } else {
+                MainActivity.super.onBackPressed();
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // ToDo Save File on External
+        // ToDo Upload to Server
+        // ToDo Save Objects when App is closed
+        if(DEBUGMODE) Log.d(TAG, "onStop");
+        FileLoader fl = new FileLoader(this);
+//        fl.saveLogsOnExternal();
+//        saveCurrentState();
+//        startActivity(new Intent(this, MainActivity.class));
     }
 
 
 
 
-    private void initResetRecordedData() {
+
+
+
+
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            Log.i("MyLauncher", "onNewIntent: HOME Key");
+
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ( keyCode == KeyEvent.KEYCODE_MENU ) {
+            Log.d(TAG, "MENU pressed");
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*****************************
+     * Init
+     *******************************************/
+
+
+
+
+
+
+    private void initConfiguration() {
+        // Init the Data Structure - there all the created where hosted
+        DataManager.init();
+        Variables.init();
+        var = Variables.getInstance();
+
+        FileLoader fl = new FileLoader(this);
+        fl.initFiles();
+    }
+
+
+
+
+    private void initCalenderMap() {
+
+        Calendar cal = Calendar.getInstance();
+        Date time = cal.getTime();
+
+        // Reset Time to 00:00:00
+        time.setHours(var.startHour);
+        time.setMinutes(var.startMin);
+        time.setSeconds(var.startMin);
+
+        // Set Calendar with reseted time
+        cal.setTime(time);
 
 
         Calendar calEndTime = Calendar.getInstance();
         Date endTime = calEndTime.getTime();
-        endTime.setSeconds(00);
-        endTime.setMinutes(30);
-        endTime.setHours(23);
+        endTime.setHours(var.endHour);
+        endTime.setMinutes(var.startMin);
+        endTime.setSeconds(var.startMin);
+        calEndTime.setTime(endTime);
+        calEndTime.add(Calendar.MINUTE, -15);
+        endTime = calEndTime.getTime();
 
 
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-
-                DataManager.getInstance().activeList = new ArrayList<>();
-                DataManager.getInstance().calenderMap = new TreeMap<>();
-                initConfiguration();
-                Log.d(TAG, "restart");
-            }
-        };
-        timer.schedule(timerTask, endTime.getTime());
-
+        while(time.before(endTime)) {
+            time = cal.getTime();
+//            Log.d(TAG, "startTime "+  time);
+            DataManager.getInstance().setCalenderMapEntry(time.toString(), null);
+            // add 15 minutes to setTime
+            cal.add(Calendar.MINUTE, 15);
+            var.startMin++;
+        }
     }
 
 
-    Handler handler = new Handler();
+
 
     private void initDataLogger() {
 
-       final FileLoader fl = new FileLoader(this);
+        final FileLoader fl = new FileLoader(this);
 
-       Timer timer = new Timer();
-       TimerTask timerTask = new TimerTask() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 final Date currentDate = Calendar.getInstance().getTime();
@@ -150,21 +268,109 @@ public class MainActivity extends Activity  {
     }
 
 
+    private void initResetRecordedData() {
+
+
+        Calendar calEndTime = Calendar.getInstance();
+        Date endTime = calEndTime.getTime();
+        endTime.setSeconds(00);
+        endTime.setMinutes(30);
+        endTime.setHours(23);
+
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+                DataManager.getInstance().activeList = new ArrayList<>();
+                DataManager.getInstance().calenderMap = new TreeMap<>();
+                initConfiguration();
+                Log.d(TAG, "restart");
+            }
+        };
+        timer.schedule(timerTask, endTime.getTime());
+
+    }
+
+
+    private void initLayout() {
+        setFullScreen(true);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Set App to fullscreen mode if boolean == true
+     * @param fullscreen    a flag for setting the app in fullscreen mode or not
+     */
+    private void setFullScreen(boolean fullscreen) {
+        if (fullscreen) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+
+
+
+    private void saveCurrentState() {
+
+        SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+
+
+        // Save ObjectActivity Map
+        Map<String, ActivityObject> map = DataManager.getInstance().getObjectMap();
+        String json = gson.toJson(map);
+        prefsEditor.putString(ACTIVITY_STATE, json);
+
+
+        // Save ActiveList
+        ArrayList<String> activeList = DataManager.getInstance().activeList;
+        json = gson.toJson(activeList);
+        prefsEditor.putString(ACTIVE_LIST, json);
+
+
+        // Save CalendarMap
+        TreeMap<String, ArrayList<String>> calendarMap = DataManager.getInstance().calenderMap;
+        json = gson.toJson(calendarMap);
+        prefsEditor.putString(CALENDAR_MAP, json);
+
+        prefsEditor.commit();
+    }
+
+
+
+
     private void loadSavedObjectState() {
         Gson gson = new Gson();
         SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
 
 
         SharedPreferences.Editor editor = mPrefs.edit();
-        editor.remove("MyObject");
-        editor.remove("ActiveList");
-        editor.remove("CalendarMap");
+        editor.remove(ACTIVITY_STATE);
+        editor.remove(ACTIVE_LIST);
+        editor.remove(CALENDAR_MAP);
         editor.apply();
 
 
-        if(mPrefs.contains("MyObject")){
+        if(mPrefs.contains(ACTIVITY_STATE)){
 
-            String json = mPrefs.getString("MyObject", "");
+            String json = mPrefs.getString(ACTIVITY_STATE, "");
             Log.d(TAG, "Jsonnnnnn " + json);
 
 
@@ -174,16 +380,16 @@ public class MainActivity extends Activity  {
             DataManager.getInstance().activityMap = newMap;
 
 
-            if(mPrefs.contains("ActiveList")){
-                json = mPrefs.getString("ActiveList", "");
+            if(mPrefs.contains(ACTIVE_LIST)){
+                json = mPrefs.getString(ACTIVE_LIST, "");
                 Type type = new TypeToken<List<String>>(){}.getType();
                 ArrayList<String> activeList = gson.fromJson(json, type);
                 DataManager.getInstance().activeList = activeList;
 
             }
 
-            if(mPrefs.contains("CalendarMap")){
-                json = mPrefs.getString("CalendarMap", "");
+            if(mPrefs.contains(CALENDAR_MAP)){
+                json = mPrefs.getString(CALENDAR_MAP, "");
                 Log.d(TAG, "Jsonnnnnn " + json);
                 Type type = new TypeToken<TreeMap<String, ArrayList<String>>>(){}.getType();
                 TreeMap<String, ArrayList<String>> calendarMap = gson.fromJson(json, type);
@@ -198,190 +404,5 @@ public class MainActivity extends Activity  {
             Variables.getInstance().activeCount = DataManager.getInstance().activeList.size();
         }
     }
-
-    // Do nothing on backpress - for ui reason - simplify navigation
-    @Override
-    public void onBackPressed() {
-        // Your Code Here. Leave empty if you want nothing to happen on back press.
-    }
-
-
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        // ToDo Save File on External
-        // ToDo Upload to Server
-        // ToDo Save Objects when App is closed
-        if(DEBUGMODE) Log.d(TAG, "onStop");
-        FileLoader fl = new FileLoader(this);
-//        fl.saveLogsOnExternal();
-//        saveCurrentState();
-//        startActivity(new Intent(this, MainActivity.class));
-    }
-
-
-
-
-    private void setFullScreen(boolean fullscreen) {
-
-        if (fullscreen) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    /*****************************
-     * Init
-     *******************************************/
-
-    /**
-     * Checks if the user started the app for the first time. If true he is redirected to
-     * the IntroActivity.
-     */
-    private void initFirstStart() {
-        boolean firstStart = Settings.isFirstRun(getBaseContext());
-
-        // did the user start the app for the first time?
-        if(firstStart) {
-            // true, show intro
-            Intent intentIntro = new Intent(this, IntroActivity.class);
-            startActivity(intentIntro);
-        }
-    }
-
-
-
-
-    private void initConfiguration() {
-        // Init the Data Structure - there all the created where hosted
-//        DataManager.init();
-//        Variables.init();
-//
-//        FileLoader fl = new FileLoader(this);
-//        fl.initFiles();
-    }
-
-
-
-
-    private void initCalenderMap() {
-
-        Calendar cal = Calendar.getInstance();
-        Date time = cal.getTime();
-
-        // Reset Time to 00:00:00
-        time.setHours(startHour);
-        time.setMinutes(startMin);
-        time.setSeconds(startMin);
-
-        // Set Calendar with reseted time
-        cal.setTime(time);
-
-
-        Calendar calEndTime = Calendar.getInstance();
-        Date endTime = calEndTime.getTime();
-        endTime.setHours(endHour);
-        endTime.setMinutes(startMin);
-        endTime.setSeconds(startMin);
-        calEndTime.setTime(endTime);
-        calEndTime.add(Calendar.MINUTE, -15);
-        endTime = calEndTime.getTime();
-
-
-        while(time.before(endTime)) {
-            time = cal.getTime();
-//            Log.d(TAG, "startTime "+  time);
-            DataManager.getInstance().setCalenderMapEntry(time.toString(), null);
-            // add 15 minutes to setTime
-            cal.add(Calendar.MINUTE, 15);
-            startMin++;
-        }
-    }
-
-    /**
-     * Checks for updates, e.g. overwrites the activites.json if it was changed on the server.
-     * Also pushes new logged data to the server.
-     */
-    private void syncWithServer() {
-//        new PullActivitiesTask(this).execute();
-//        new PushActivitiesTask(this).execute();
-    }
-
-
-
-    private void saveCurrentState() {
-
-        SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor prefsEditor = mPrefs.edit();
-        Gson gson = new Gson();
-
-        // Save ObjectActivity Map
-        Map<String, ActivityObject> map = DataManager.getInstance().getObjectMap();
-        String json = gson.toJson(map);
-        prefsEditor.putString("MyObject", json);
-
-
-        // Save ActiveList
-        ArrayList<String> activeList = DataManager.getInstance().activeList;
-        json = gson.toJson(activeList);
-        prefsEditor.putString("ActiveList", json);
-
-
-        // Save CalendarMap
-        TreeMap<String, ArrayList<String>> calendarMap = DataManager.getInstance().calenderMap;
-        json = gson.toJson(calendarMap);
-        prefsEditor.putString("CalendarMap", json);
-
-        prefsEditor.commit();
-    }
-
-
-
-
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
-            Log.i("MyLauncher", "onNewIntent: HOME Key");
-
-        }
-    }
-
-
-
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//
-//        Log.d(TAG, "event " + event.toString() + " " + keyCode);
-//
-//        if ((keyCode == KeyEvent.KEYCODE_MENU)) {
-//            Log.d(TAG, "Menu");
-////            return true;
-//        }
-////        return super.onKeyDown(keyCode, event);
-//        return true;
-//    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ( keyCode == KeyEvent.KEYCODE_MENU ) {
-            Log.d(TAG, "MENU pressed");
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
 
 }
